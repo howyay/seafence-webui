cbb8539fb13db487060aff2359104f8bbfb9cbe1
Haoye <haoyetang@gmail.com>
2023-08-18 16:13:24 -0400

    Init repo

---
+++ test.js
@@ -0,0 +1,149 @@
+var haveEvents = 'GamepadEvent' in window;
+var haveWebkitEvents = 'WebKitGamepadEvent' in window;
+var controllers = {};
+var rAF = window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.requestAnimationFrame;
+
+function connecthandler(e) {
+  addgamepad(e.gamepad);
+}
+function addgamepad(gamepad) {
+  controllers[gamepad.index] = gamepad; var d = document.createElement("div");
+  d.setAttribute("id", "controller" + gamepad.index);
+  var t = document.createElement("h1");
+  t.appendChild(document.createTextNode("gamepad: " + gamepad.id));
+  d.appendChild(t);
+  var b = document.createElement("div");
+  b.className = "buttons";
+  for (var i=0; i<gamepad.buttons.length; i++) {
+    var e = document.createElement("span");
+    e.className = "button";
+    //e.id = "b" + i;
+    e.innerHTML = i;
+    b.appendChild(e);
+  }
+  d.appendChild(b);
+  var a = document.createElement("div");
+  a.className = "axes";
+  for (i=0; i<gamepad.axes.length; i++) {
+    e = document.createElement("meter");
+    e.className = "axis";
+    //e.id = "a" + i;
+    e.setAttribute("min", "-1");
+    e.setAttribute("max", "1");
+    e.setAttribute("value", "0");
+    e.innerHTML = i;
+    a.appendChild(e);
+  }
+  d.appendChild(a);
+  document.getElementById("start").style.display = "none";
+  document.body.appendChild(d);
+  rAF(updateStatus);
+}
+
+function disconnecthandler(e) {
+  removegamepad(e.gamepad);
+}
+
+function removegamepad(gamepad) {
+  var d = document.getElementById("controller" + gamepad.index);
+  document.body.removeChild(d);
+  delete controllers[gamepad.index];
+}
+
+function updateStatus() {
+  scangamepads();
+  for (j in controllers) {
+    var controller = controllers[j];
+    var d = document.getElementById("controller" + j);
+    var buttons = d.getElementsByClassName("button");
+    for (var i=0; i<controller.buttons.length; i++) {
+      var b = buttons[i];
+      var val = controller.buttons[i];
+      var pressed = val == 1.0;
+      var touched = false;
+      if (typeof(val) == "object") {
+        pressed = val.pressed;
+        if ('touched' in val) {
+          touched = val.touched;
+        }
+        val = val.value;
+      }
+      var pct = Math.round(val * 100) + "%";
+      b.style.backgroundSize = pct + " " + pct;
+      b.className = "button";
+      if (pressed) {
+        b.className += " pressed";
+        buttonHandler(i, pct);
+      }
+      if (touched) {
+        b.className += " touched";
+      }
+    }
+
+    var axes = d.getElementsByClassName("axis");
+    for (var i=0; i<controller.axes.length; i++) {
+      var a = axes[i];
+      a.innerHTML = i + ": " + controller.axes[i].toFixed(4);
+      a.setAttribute("value", controller.axes[i]);
+      axesHandler(i, controller.axes[i]);
+    }
+  }
+  rAF(updateStatus);
+}
+
+function scangamepads() {
+  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
+  for (var i = 0; i < gamepads.length; i++) {
+    if (gamepads[i] && (gamepads[i].index in controllers)) {
+      controllers[gamepads[i].index] = gamepads[i];
+    }
+  }
+}
+
+if (haveEvents) {
+  window.addEventListener("gamepadconnected", connecthandler);
+  window.addEventListener("gamepaddisconnected", disconnecthandler);
+} else if (haveWebkitEvents) {
+  window.addEventListener("webkitgamepadconnected", connecthandler);
+  window.addEventListener("webkitgamepaddisconnected", disconnecthandler);
+} else {
+  setInterval(scangamepads, 500);
+}
+
+function axesHandler(axis, value) {
+  if (Math.abs(value) < 0.3) return;
+  console.log("Axis " + axis + "registering " + value + "% of maximum input");
+  switch (axis) {
+    case 1:
+      if (value > 0)
+      command.textContent="Opening gripper. Servo PWM " + value * 100 +"% duty cyle."
+      else if (value < 0)
+      command.textContent="Closing gripper. Servo PWM " + value * 100 + "% dutycyle";
+    case 2:
+      if (value > 0)
+      command.textContent="Horizontal component registered, accelerating left motor to turn right";
+      else 
+      command.textContent="Horizontal component registered, accelerating right motor to turn left";
+    case 3:
+      if (value > 0)
+      command.textContent="Decelerating.";
+      else 
+      command.textContent="Accelerating."; 
+  }
+}
+
+function buttonHandler(button, percent) {
+  console.log("Button click on " + button + " registered.");
+  status = document.getElementById("status");
+  command = document.getElementById("command");
+  status.textContent="Button press event dispatched, sending control signal over socket:";
+  switch (button) {
+    case 6:
+      command.textContent=": Descending. Ballast valve flow rate " + percent;
+    case 7:
+      command.textContent="Ascending. Ballast valve flow rate " + percent;
+  }
+}
+
